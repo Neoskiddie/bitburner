fd5f18d6f6dad2c245582c1c590befa976c349e8
anon <anon@pop-os.localdomain>
2022-12-30 09:55:55 +0000

    Added file with commands to buy all exploits
     * Added file with commands to buy all exploits
     * Foixed the deployToBot script

--- deployToBot.js
+++ deployToBot.js
@@ -1,7 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
 	const homeDeployScript = "home-deploy.js";
-	const createScript = "createScript.js";
+	const createScript = "createScripts.js";
 	if (ns.args[0] == null) {
 		ns.tprint("Pass server to deploy to as first argument");
 		return;
