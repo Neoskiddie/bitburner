80dfa960fe906bf5e9d304fc770cec2ad163508e
anon <anon@windows>
2023-01-07 23:20:36 +0000

    Small fixes to mega scan
    Added a bit more functionality. Now a range of scan can be specified

--- megaScan.js
+++ megaScan.js
@@ -1,33 +1,43 @@
 // Simple scan formatted with visual depth of scan
 export async function main(ns) {
-	ns.tprint(scanRec(ns, "home", 0, true));
+	ns.tprint("You can specify level of server (hacking skill required) to search for.");
+	ns.tprint("First arg is beginning of range, second end");
+	const start = ns.args[0];
+	const end = ns.args[1];
+	ns.tprint(scanRec(ns, "home", 0, true, start, end));
 }
 
-function scanRec(ns, inputServer, depth, isFirstCall) {
+function scanRec(ns, inputServer, depth, isFirstCall, start, end) {
 	let list = [];
 	if (inputServer == null) {
 		return list;
 	}
+	let hackingLevel = ns.getServerRequiredHackingLevel(inputServer);
+	if (start != null &&
+	 	end != null &&
+		hackingLevel >= start &&
+		hackingLevel <= end) {
 
-	let hasBackdoor = ns.getServer(inputServer).backdoorInstalled;
-
-	let indentation = '-'.repeat(depth)
-	list.push(inputServer + "\n"); 
-	list.push("--Hops: " + depth + "\n");
-	list.push("--Root Access: ");
-	list.push(ns.hasRootAccess(inputServer) ? "YES\n" : "NO\n")
-	list.push("--Backdoor: ");
-	list.push(hasBackdoor ? "YES\n" : "NO\n")
-	list.push("--Required hacking skill: " + ns.getServerRequiredHackingLevel(inputServer) + "\n");
-	list.push("\n")
-	depth++;
+		let ram = ns.getServerMaxRam(inputServer);
+		let hasBackdoor = ns.getServer(inputServer).backdoorInstalled;
+		list.push(inputServer + "\n"); 
+		list.push("--Hops: " + depth + "\n");
+		list.push("--RAM: " + ram + "GB\n");
+		list.push("--Root Access: ");
+		list.push(ns.hasRootAccess(inputServer) ? "YES\n" : "NO\n")
+		list.push("--Backdoor: ");
+		list.push(hasBackdoor ? "YES\n" : "NO\n")
+		list.push("--Required hacking skill: " + hackingLevel + "\n");
+		list.push("\n")
+	}
+		depth++;
 	
 	let connectedServers = ns.scan(inputServer);
 	if (!isFirstCall) {
 		connectedServers.shift();
 	}
 	for (let x = 0; x < connectedServers.length; x++) {
-		list.push(scanRec(ns, connectedServers[x], depth, false));
+		list.push(scanRec(ns, connectedServers[x], depth, false, start, end));
 	}
 
 	return list.join('');
