85748486fff359e6c5d3e59327f2c82c68b41b73
anon <anon@windows>
2023-01-07 23:09:38 +0000

    Modified connect.js
    Modified connect.js with a bit of code that enters the command
    into the command prompt.
    Unfortunately it's unable to press enter for the user.

--- connect.js
+++ connect.js
@@ -1,6 +1,6 @@
 export async function main(ns) {
 	if (ns.args.length != 1) {
-		ns.tprint("Please specify server you want to connect to.");
+		ns.tprintf("Please specify server you want to connect to.");
 	}
 	const host = ns.args[0];
 	let serverList = [];
@@ -13,7 +13,7 @@ export async function main(ns) {
 	}
 
 	serverList.reverse(); // reverse the array - so in the order we want to connect
-	printCommand(ns, serverList);
+	inputCommands(ns, getCommand(ns, serverList));
 }
 
 function getPreviousServer(ns, host) {
@@ -21,12 +21,30 @@ function getPreviousServer(ns, host) {
 	return scanResult[0];// first server is always the previous server
 }
 
-function printCommand(ns, listOfServers) {
+function getCommand(ns, listOfServers) {
 
 	let command = "home;"; //add home as first command just in case script is run from other server
 	for (let x = 0; x < listOfServers.length; x++) {
 		let server = listOfServers[x];
 		command += "connect " + server + ";";
 	}
-	ns.tprint("\n" + command);
+	return command;
+}
+
+// hacky bit of JS, only part of code not written by me
+// found on https://www.reddit.com/r/Bitburner/comments/sljpi4/got_bored_to_buy_the_scripts_after_a_reset/
+// It doesn't entirely work in Steam version - cannot enter.
+// But it at least outputs straight to terminal
+function inputCommands(ns, input) {
+	let terminalInput = ''
+	eval('terminalInput = document.getElementById("terminal-input")')
+	if (! terminalInput) {
+		ns.toast('!!! You need to be in terminal window !!!', 'error')
+		return ;
+	}
+
+	terminalInput.value = input;
+	const handler = Object.keys(terminalInput)[1];
+	terminalInput[handler].onChange({ target: terminalInput });
+	terminalInput[handler].onKeyDown({ keyCode: 13, preventDefault: () => null });
 }
